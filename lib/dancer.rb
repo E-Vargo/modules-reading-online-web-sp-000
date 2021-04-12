68f2b2f1a92541642139f3d8f427ca5862039cba
Elliott Vargo <vargoelliott@gmail.com>
2021-04-12 00:54:19 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,10 +2,11 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-include Dance
-attr_accessor :name
+  include Dance
 
- def initialize(name)
-   @name = name
- end
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
